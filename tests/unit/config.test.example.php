c0f9949fbb0217e3dcad4323d523a124904c359c
Lars Olesen <lars@intraface.dk>
2010-06-07 22:13:03 +0000

    More sensible defaults.

--- tests/unit/config.test.example.php
+++ tests/unit/config.test.example.php
@@ -1,15 +1,17 @@
 <?php
+error_reporting(E_ALL &~E_DEPRECATED);
+
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'vih');
 define('DB_PASSWORD', '');
-define('DB_PASS', '');
+define('DB_PASS', DB_PASSWORD);
 define('DB_USER', 'root');
 define('DB_DSN', 'mysql://' . DB_USER . ':' . DB_PASSWORD . '@' . DB_HOST . '/' . DB_NAME);
 
 define('PATH_ROOT', '');
 define('PATH_WWW', '');
 
-set_include_path('c:/Users/Lars Olesen/workspace/vih/hojskole/src/' . PATH_SEPARATOR . get_include_path());
+set_include_path(dirname(__FILE__) . '/../../src/' . PATH_SEPARATOR . get_include_path());
 
 require_once 'Ilib/ClassLoader.php';
 require_once 'VIH/configuration.php';
