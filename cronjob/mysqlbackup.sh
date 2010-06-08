7ca135093afd6bd967f793f3f9d2d258e17cd4c6
Lars Olesen <lars@intraface.dk>
2010-06-08 16:52:59 +0000

    Removed the password.

--- cronjob/mysqlbackup.sh
+++ cronjob/mysqlbackup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash/
-mysqldump --user=vih --password=qdq65txp --host=mysql.vih.dk vih > /home/vih/backup/mysql/vih.sql
+mysqldump --user=vih --password=x --host=mysql.vih.dk vih > /home/vih/backup/mysql/vih.sql
 tar -zcvf /home/vih/backup/mysql/vih_sql.tar.gz /home/vih/backup/mysql/vih.sql
 date | mutt -s "vih mysql backup" -a /home/vih/backup/mysql/vih_sql.tar.gz lars@vih.dk
