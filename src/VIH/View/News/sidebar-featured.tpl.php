35d6b63fd1ff1f79ae6429ed571085968e6df139
Lars Olesen <lars@intraface.dk>
2010-10-05 09:07:33 +0200

    Now strip_tags() before printing the teaser.

--- src/VIH/View/News/sidebar-featured.tpl.php
+++ src/VIH/View/News/sidebar-featured.tpl.php
@@ -1,4 +1,4 @@
 <?php foreach ($nyheder as $nyhed): ?>
     <h3><a href="<?php e(url('/nyheder/' . $nyhed->get('id'))); ?>"><?php e($nyhed->get('overskrift')); ?></a></h3>
-    <p><?php e($nyhed->get('teaser')); ?></p>
+    <p><?php e(strip_tags($nyhed->get('teaser'))); ?></p>
 <?php endforeach; ?>
