c51e2badc22e5bd484053391909b5e43e20af03a
Sune Jensen <sune@asterix.site>
2011-01-27 22:13:31 +0100

    updated conditions and moved html to template

--- src/VIH/Controller/KortKursus/Tilmelding/Conditions.php
+++ src/VIH/Controller/KortKursus/Tilmelding/Conditions.php
@@ -1,57 +1,19 @@
 <?php
 class VIH_Controller_KortKursus_Tilmelding_Conditions extends k_Component
 {
+    protected $template;
+
+    function __construct(k_TemplateFactory $template)
+    {
+        $this->template = $template;
+    }
+    
     function renderHtml()
     {
-        $this->document->setTitle('Betalings- og forsikringsbetingelser');
-        return '
-        <h1>Betingelser</h1>
+        $this->document->setTitle('Betalingsbetingelser');
+        
+        $tpl = $this->template->create('KortKursus/Tilmelding/conditions');
 
-        <p><a href="'.$this->url('../confirm').'">Tilbage</a></p>
-
-        <h3>Betaling</h3>
-
-        <p><strong>Tilmeldingsdepositum</strong> indbetales samtidig med indsendelse af tilmeldingsblanketten. Tilmeldingsdepositummet tilbagebetales ikke ved framelding med mindre man har tegnet en afbestillingsforsikring hos Vejle Idrætshøjskole.</p>
-        <p><strong>Det resterende beløb</strong> (kursuspris minus tilmeldingsdepositum) indbetales senest 6 uger før kursusstart.</p>
-
-        <p>Alle beløb er danske kroner.</p>
-
-        <h3>Betalingsformer</h3>
-
-        <p>Du kan betale på en af følgende måder:</p>
-
-        <ul>
-            <li>Overføre pengene til kontonummer <strong>7244-1469664</strong></li>
-            <li>Dankort <img src="'.$this->url('/gfx/icons/dankort.jpg').'" alt="Dankort" /></li>
-        </ul>
-
-        <h3>Framelding</h3>
-
-        <h4>Vejle Idrætshøjskoles afbestillingsforsikring</h4>
-
-        <p>Afbestillingsforsikringen skal bestilles og betales i forbindelse med tilmeldingen (husk at sætte kryds for forsikring på tilmeldingsblanketten).</p>
-
-        <ul>
-            <li>Forsikringen skal tegnes og betales ved tilmelding (kan ikke efterbestilles).</li>
-            <li>Ved afbud refunderes hele det indbetalte kursusbeløb (på nær forsikringspræmien).</li>
-        </ul>
-
-        <h4>Framelding uden forsikring</h4>
-        <ul>
-            <li>Ved framelding inden 14 dage før kursusstart får man kursusprisen retur på nær tilmeldingsdepositummet.</li>
-            <li>Ved framelding senere end 14 dage før kursusstart er kursusbeløbet tabt.</li>
-        </ul>
-
-        <h3>Kontakt</h3>
-        <p>
-        Vejle Idrætshøjskole
-        <br />Ørnebjergvej 28
-        <br />7100 Vejle
-        <br />Telefon: 75820811
-        <br />E-mail: kontor@vih.dk
-        <br />CVR: 36850728
-        </p>
-        ';
-
+        return $tpl->render($this);
     }
 }
---
+++ src/VIH/View/KortKursus/Tilmelding/conditions.tpl.php
@@ -0,0 +1,43 @@
+<h1>Betingelser</h1>
+
+<p><a href="'.$this->url('../confirm').'">Tilbage</a></p>
+
+<h3>Tilmelding</h3>
+
+<p><strong>Tilmeldingsgebyr</strong> kr. 1.000,- pr. deltager indbetales samtidig med tilmelding til et kursus. Tilmeldingen er først gyldig, når skolen har modtaget din tilmelding og dit depositum.</p>
+
+<h3>Bekræftelse</h3>
+<p>Når skolen har modtaget tilmelding og depositum, sendes en bekræftelse på kursusdeltagelsen.</p>
+
+<h3>Betalingsformer</h3>
+
+<p>Du kan betale på en af følgende måder:</p>
+
+<ul>
+    <li>Overføre pengene til kontonummer <strong>7244-1469664</strong></li>
+    <li>Dankort <img src="'.$this->url('/gfx/icons/dankort.jpg').'" alt="Dankort" /></li>
+</ul>
+
+<p><strong>Husk</strong> at oplyse reservationsnummer, kursustitel og navn.</p>
+
+<p><strong>Det resterende beløb</strong> (kursuspris minus tilmeldingsdepositum) indbetales via netbank senest 10 uger før kursusstart.</p>
+
+<p>Alle beløb er danske kroner.</p>
+
+<h3>Framelding</h3>
+<ul>
+    <li>Ved framelding mere end 30 dage før kursusstart får man kursusgebyret retur på nær TILMELDINGSGEBYR på kr. 1.000,-</li>
+    <li>Ved framelding mindre end 30 dage og mere end 14 dage før kursusstart er 50 % af kursusgebyret tabt.</li>
+    <li>Ved framelding mindre en 14 dage tabes hele kursusgebyret.</li>
+</ul>
+
+
+<h3>Kontakt</h3>
+<p>
+Vejle Idrætshøjskole
+<br />Ørnebjergvej 28
+<br />7100 Vejle
+<br />Telefon: 75820811
+<br />E-mail: kontor@vih.dk
+<br />CVR: 36850728
+</p>
