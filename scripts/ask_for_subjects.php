71c11dd9db299c082cd6885b237f0d9f122e4b82
Lars Olesen <lars@intraface.dk>
2011-06-25 23:38:00 +0200

    Minor changes to the script that asks students to update their subjects

--- scripts/ask_for_subjects.php
+++ scripts/ask_for_subjects.php
@@ -1,13 +1,14 @@
 <?php
-//die('only run after setting up the date');
 require_once 'config.local.php';
-set_include_path(PATH_INCLUDE);
 require_once 'Ilib/ClassLoader.php';
 
 $date = date('Y-m-d');
 
 $db = MDB2::factory(DB_DSN);
-$res = $db->query('SELECT langtkursus_tilmelding.id FROM langtkursus_tilmelding INNER JOIN langtkursus ON langtkursus.id = langtkursus_tilmelding.kursus_id WHERE langtkursus.dato_start > "'.$date.'" AND langtkursus.active = 1 AND langtkursus_tilmelding.active = 1');
+$res = $db->query('SELECT langtkursus_tilmelding.id 
+    FROM langtkursus_tilmelding 
+        INNER JOIN langtkursus ON langtkursus.id = langtkursus_tilmelding.kursus_id 
+    WHERE langtkursus.dato_start > "'.$date.'" AND langtkursus.active = 1 AND langtkursus_tilmelding.active = 1');
 if (PEAR::isError($res)) {
     die($res->getUserInfo());
 }
@@ -16,10 +17,10 @@ while ($row = $res->fetchRow(MDB2_FETCHMODE_ASSOC)) {
     $tilmelding = new VIH_Model_LangtKursus_Tilmelding($row['id']);
     $link = 'http://vih.dk/login/langekurser/' .  $tilmelding->get('code');
     $subject = 'Tjek dine fag';
-    $body = 'Du er meldt til et af de kommende kurser på Vejle Idrætshøjskole. Vi sidder midt i planlægningen, og vi har brug for at vide, hvilke idrætsspecialer og linjer, du gerne vil have. Derfor beder vi dig gå ind på nedenstående link og tjekke om de rigtige fag er krydset af. Du skal vælge et fag fra hver faggruppe, og du finder linket til Dine fag nederst på siden.' . "\n\n" . $link . "\n\nVær opmærksom på, at det skyldige beløb ikke nødvendigvis er rigtigt regnet ud endnu. Det gør vores kontor hurtigst muligt. Hvis du har nogen spørgsmål, er du meget velkommen til at skrive til lars@vih.dk.\n\nVenlig hilsen\n\nLars Olesen\nVejle Idrætshøjskole";
+    $body = 'Du er meldt til et af de kommende kurser på Vejle Idrætshøjskole. Vi sidder midt i planlægningen, og vi har brug for at vide, hvilke fag, du gerne vil have. Derfor beder vi dig gå ind på nedenstående link og tjekke om de rigtige fag er krydset af. Du skal vælge et fag fra hver faggruppe, og du finder linket til Rediger fag nederst på siden.' . "\n\n" . $link . "\n\nVær opmærksom på, at det skyldige beløb ikke nødvendigvis er rigtigt regnet ud endnu. Det gør vores kontor hurtigst muligt. Hvis du har nogen spørgsmål, er du meget velkommen til at skrive til henriette@vih.dk.\n\nVenlig hilsen\n\nVejle Idrætshøjskole";
 
-    $headers = 'From: lars@vih.dk' . "\r\n" .
-    'Reply-To: lars@vih.dk';
+    $headers = 'From: henriette@vih.dk' . "\r\n" .
+    'Reply-To: henriette@vih.dk';
 
     if ($email = $tilmelding->get('email')) {
         mail($email, $subject, $body, $headers);
